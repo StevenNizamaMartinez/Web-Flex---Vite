e3863a580666d69eec6655bfbca22c4768bb7fe2
Steve Farit Nizama Martinez <0331172014@unjfsc.edu.pe>
2023-01-27 16:16:16 -0500

    se arreglo el desplazamiento

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+
+
 const buttonNav = document.querySelector('.bar')
 
 buttonNav.addEventListener('click', 
     () => {
     document.querySelector('.nav-bar-right').classList.toggle('enable');
+    document.addEventListener('touchmove', e => e.preventDefault(), { passive: false });
     }
-)
+    )
+
+    buttonNav.addEventListener('click', 
+    () => {
+    }
+    )
