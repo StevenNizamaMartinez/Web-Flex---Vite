87c15d82a1112f68fcded883ff995b2a0b14ade4
Steve Farit Nizama Martinez <0331172014@unjfsc.edu.pe>
2023-01-27 16:48:23 -0500

    add toggle

--- main.js
+++ main.js
@@ -5,11 +5,6 @@ const buttonNav = document.querySelector('.bar')
 buttonNav.addEventListener('click', 
     () => {
     document.querySelector('.nav-bar-right').classList.toggle('enable');
-    document.addEventListener('touchmove', e => e.preventDefault(), { passive: false });
-    }
-    )
-
-    buttonNav.addEventListener('click', 
-    () => {
+    document.addEventListener('touchmove', e => e.preventDefault.toggle(), { passive: false });
     }
     )
